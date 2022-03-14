fe16b06c2f2c77e0d8289ea6f39ec57449344de1
joseRbrandao <jose.brandao@softrends.io>
2022-03-14 15:06:34 -0300

    defaultOptions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Dogs from "./Pages/Dogs";
 
+const defaultOptions = {
+  //# stable configurations
+  port: 45678,
+  source: "build",
+  destination: null,
+  concurrency: 4,
+  include: ["/"],
+  userAgent: "ReactSnap",
+  // 4 params below will be refactored to one: `puppeteer: {}`
+  // https://github.com/stereobooster/react-snap/issues/120
+  headless: true,
+  puppeteer: {
+    cache: true,
+  },
+  puppeteerArgs: [],
+  // puppeteerExecutablePath: undefined,
+  puppeteerExecutablePath: "/usr/bin/google-chrome-unstable",
+};
+
 const App = () => (
   <BrowserRouter>
     <div className="App">
