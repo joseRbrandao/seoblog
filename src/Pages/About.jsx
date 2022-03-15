971552e4c5f7aa6d10fe501513e06beaeae66d16
joseRbrandao <jose.brandao@softrends.io>
2022-03-15 10:20:48 -0300

    page about - insert react-helmet

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,16 +1,28 @@
+import { Helmet } from "react-helmet";
+
 const About = () => {
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100%",
-        background: "blue",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      Page About
+    <div>
+      <Helmet>
+        <title>Blog para testar praticas de SEO</title>
+        <meta
+          name="description"
+          content="estudo de caso sobre SEO. metatags, title, description, react-helmet, reat-router-dom, 
+              google search console, site map, robot.tsx."
+        />
+      </Helmet>
+      <div
+        style={{
+          height: "100vh",
+          width: "100%",
+          background: "blue",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Page About
+      </div>
     </div>
   );
 };
