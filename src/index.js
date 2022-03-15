b1489fb932325ba8cd536e4d30c1e4702b64fcdc
joseRbrandao <jose.brandao@softrends.io>
2022-03-15 09:21:56 -0300

    remove react-snap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
-import { hydrate, render } from "react-dom";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
