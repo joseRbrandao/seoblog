04d08ecb7ba83c314902ace929f9209d138aab9a
joseRbrandao <jose.brandao@softrends.io>
2022-03-15 10:00:22 -0300

    insert metadescription e title. React helmet

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,15 @@
+import { Helmet } from "react-helmet";
+
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>Estudando SEO e suas melhores práticas</title>
+        <meta
+          name="description"
+          content="Aqui eu estou testanto a descrição META e se está funcionando"
+        />
+      </Helmet>
       <span
         style={{
           height: "100vh",
