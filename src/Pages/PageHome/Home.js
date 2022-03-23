edaedd4db8d1c11333860b1d969ab2d3ab8d8c38
joseRbrandao <jose.brandao@softrends.io>
2022-03-23 10:58:14 -0300

    Remove Matomo. Add Pausive

--- src/Pages/PageHome/Home.js
+++ src/Pages/PageHome/Home.js
@@ -12,23 +12,6 @@ import {
 import iconseo from "../../assets/seo.svg";
 
 const Home = () => {
-  var _paq = (window._paq = window._paq || []);
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-  _paq.push(["setCookieDomain", "*.misty-fog-5874.on.fleek.co"]);
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-  (function () {
-    var u = "https://fleek.matomo.cloud/";
-    _paq.push(["setTrackerUrl", u + "matomo.php"]);
-    _paq.push(["setSiteId", "1"]);
-    var d = document,
-      g = d.createElement("script"),
-      s = d.getElementsByTagName("script")[0];
-    g.async = true;
-    g.src = "//cdn.matomo.cloud/fleek.matomo.cloud/matomo.js";
-    s.parentNode.insertBefore(g, s);
-  })();
   return (
     <>
       <Helmet>
